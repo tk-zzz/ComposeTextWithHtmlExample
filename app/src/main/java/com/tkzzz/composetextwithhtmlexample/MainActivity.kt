24989e55dbf08fdf2c67ebf3fd38f0d8a49a06c5
tk-zzz <49810315+tk-zzz@users.noreply.github.com>
2024-06-27 17:12:17 +0900

    listenerおいてみたけどクリックできなかった

--- app/src/main/java/com/tkzzz/composetextwithhtmlexample/MainActivity.kt
+++ app/src/main/java/com/tkzzz/composetextwithhtmlexample/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.tkzzz.composetextwithhtmlexample
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
@@ -11,9 +13,16 @@ import androidx.compose.foundation.text.BasicText
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.currentCompositionLocalContext
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.LinkAnnotation
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextLinkStyles
 import androidx.compose.ui.text.fromHtml
 import androidx.compose.ui.tooling.preview.Preview
 import com.tkzzz.composetextwithhtmlexample.ui.theme.ComposeTextWithHtmlExampleTheme
@@ -43,12 +52,29 @@ fun TextWithHtml(modifier: Modifier = Modifier) {
 //    val string = """<a href="https://reality.app"> REALITY </a> へようこそ！！！！！！<br>123456<big><font color="RED">7890</font></big>1234567890""" // resourceを経由しないならHTMLエスケープはしなくてよい
 //    val string = stringResource(id = R.string.tagged_text) // こっちはHtmlエスケープしてないリソースなので反映されない。
     val string = stringResource(id = R.string.tagged_text_with_escape)
-    val styledAnnotatedString = AnnotatedString.fromHtml(htmlString = string)
+//    val string = """<a href="https://hoge.com"> REALITY</a>ggggg"""
+    val context = LocalContext.current
 
+    val localUriHandler = LocalUriHandler.current
+    val styledAnnotatedString = AnnotatedString.fromHtml(
+        htmlString = string,
+        linkStyles = TextLinkStyles(
+            style = SpanStyle(
+                color = Color.Blue,
+            )
+        ),
+        linkInteractionListener = { annotation ->
+            Toast.makeText(context,"hoge", Toast.LENGTH_SHORT).show()
+            val url = annotation as? LinkAnnotation.Url ?: return@fromHtml
+            localUriHandler.openUri(url.url)
+        }
+    )
 
-    Text(
+    BasicText(
         text = styledAnnotatedString,
-        modifier = modifier,
+//        modifier = Modifier.clickable {
+//            Toast.makeText(context,"fuga!", Toast.LENGTH_SHORT).show()
+//        }
     )
 }
 
